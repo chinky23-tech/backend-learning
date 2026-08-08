05cd5ad8bf40e7ac67b2f93919787858e7478ef3
chinky23-tech <chinky0330@gmail.com>
2026-08-08 23:55:11 +0530

    basics

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello chinky")
+name = "chinky"
+age = 35
+height = 5.2
+city = "patiala"
+print (name)
+print (age)
+print (height)
+print(city)
+
+price = 500 
+quantity = 3
+
+print(price * quantity)
+
