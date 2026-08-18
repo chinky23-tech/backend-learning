e7eda032e63179312b8cebbf00f85f03d6c79d3f
chinky23-tech <chinky0330@gmail.com>
2026-08-18 23:23:45 +0530

    update

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-price = 50000
 
-quantity = 2
 
-cart_total = price * quantity
+products = ["laptop", "phone", "tablet", "keyboard"]
 
-if cart_total >= 50000:
-    discount = 20
-elif cart_total >= 20000:
-     discount = 10
+if product in products:
+    print("product is available")
 else:
-     discount = 0
-
-discount_amount = cart_total * discount / 100
-
-final_price = cart_total - discount_amount
-
-print("Cart Total:", cart_total)
-
-print("Discount:" , discount, "%")
-
-print("Discount amount:", discount_amount)
-
-print("final_price:", final_price)
+    print("product is not available")      
