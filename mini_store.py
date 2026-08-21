b2544363afe3df376ea9525f6886fb56e1e58f00
chinky23-tech <chinky0330@gmail.com>
2026-08-21 22:35:37 +0530

    functions

--- mini_store.py
+++ mini_store.py
@@ -1,14 +1,3 @@
-products = ["laptop" , "phone" , "tablet" , "keyboard"]
-
-print("=== MINI STORE ===")
-
-print("Avialable products:")
-
-print(products)
-
-product = input("Enter product you want")
-
-if product in products:
-    print("product is avialable")
-else:
-    print("product is not avialable")    
+def show_products():
+    print("laptop", "phone", "tablet", "keyboard")
+show_products()    
