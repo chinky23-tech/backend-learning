60abdee3d29d979f72ce8db72ec29a0abf56c45b
chinky23-tech <chinky0330@gmail.com>
2026-08-23 22:33:38 +0530

    leanrt

--- mini_store.py
+++ mini_store.py
@@ -1,8 +1,6 @@
 def calculate_total(price1, price2, price3):
     return price1 + price2 + price3
-price1 = 100
-price2 = 200
-price3 = 200
-calculate_total(100 , 200 , 200)
-total = (price1 , price2 , price3 )
+
+
+total = calculate_total(100 , 200 , 200)
 print(total)
