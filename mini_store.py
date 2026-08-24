b49e7ca1aa710784d01dc81f039e681243b8b2aa
chinky23-tech <chinky0330@gmail.com>
2026-08-24 21:46:54 +0530

    new concept

--- mini_store.py
+++ mini_store.py
@@ -4,3 +4,12 @@
 
 #total = calculate_total(100 , 200 , 200)
 #print(total)
+
+product = {"laptop" : "50000", "keyboard" : "20000"}
+def show_products(product):
+
+ for product_name, price in product.items():
+   print(product_name, price)
+
+show_products(product)
+
