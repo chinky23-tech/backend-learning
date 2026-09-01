1f02c845098ed03c867b4a78266ff3863072b63b
chinky23-tech <chinky0330@gmail.com>
2026-09-01 22:23:11 +0530

    update

--- greet.py
+++ greet.py
@@ -23,7 +23,7 @@ while True:
        ask_name(name)
        """
 #task 3
-
+"""
 def greet(name):
     print("hello" + " " + name)
 while True:
@@ -34,5 +34,29 @@ while True:
         continue
     else:
         greet(name)
+"""
+    #task 4
+products = {"pencil" : 30,"pen" :50,"copy" : 20}
+cart = []
+def check_product():
+  
+        while True:
+            product = input("Enter product :")
+            
+            if product == "quit":
+                
+                break   
+            if product not in products:
+                
+                continue 
+            quantity = int(input("Enter quantity: "))
+            cart.append([product, quantity, products[product]])
+check_product()
+total = 0
+for product_name , quantity, price in cart:
+    print(product_name, quantity, price)  
+    total += quantity * price 
 
-    
+print("total is ", total)
+
+                
