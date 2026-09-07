dabff4126c771f7ec9204e829e8c4b157c75b286
chinky23-tech <chinky0330@gmail.com>
2026-09-07 23:45:41 +0530

    update

--- dic.py
+++ dic.py
@@ -1,5 +1,5 @@
 #task 1
-
+"""
 user = {"name" : "chinky" , "age" : 35 , "city" : "Patiala"}
 user["city"] = "chandigrah"
 user["skill"] = "python"
@@ -7,3 +7,29 @@ user["email"] = "example@gmail.com"
 
 for key , value in user.items():
     print(key, ":" ,value)
+if "phone" in user:
+    print("phone exist")
+else:
+    print("phone not found")    
+    """
+#task2 
+users = {
+    "user1" : { 
+        "name" : "chinky",
+        "age" : 35,
+        "skill" : ["python" , "react"],
+
+    },
+    "user2" : {
+        "name" : "rahul",
+        "age" : 26,
+        "skill" : ["node.js" , "express.js"]
+
+    }
+}
+for user_id , user_data in users.items():
+     print(user_data["name"])
+
+for user_skill in user_data["skill"]:
+     print(user_skill)
+      
