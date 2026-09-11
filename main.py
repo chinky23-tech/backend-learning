acf32ff438cabbb7bdff9fd5af024308b532613d
chinky23-tech <chinky0330@gmail.com>
2026-09-11 22:53:59 +0530

    decorators

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def say_hi():
- print("Hi")
-def my_decorator(func):
- print("Before function")
- func()
- print("After function")
-my_decorator(say_hi)
+def login_required(func):
+    @login_required
+    def view_profile():
+     print("showing profile")
+def wrapper():  
+   logged_in = True
+
+   if logged_in:
+       func()
+   else:
+      print("please login first")      
+      return wrapper
