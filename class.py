fd786f1114af99411183ce18201978e346985d30
chinky23-tech <chinky0330@gmail.com>
2026-09-25 23:58:47 +0530

    update

--- class.py
+++ class.py
@@ -10,13 +10,22 @@ class Product:
     def total_value(self):
        return self.price * self.num
      
-    
+
+    def update_price(self, new_price):
+
+        if new_price >= 0:
+            self.price = new_price
+        else:
+            print("Price must be greater than 0")    
+
+
 
 laptop = Product("Laptop", 5000,5) 
 phone = Product("Phone", 30000, 2)
 keyboard = Product("Keyboard" ,2000, 10 )
 
-
+phone.update_price(32000)
+print(phone.price)
 
 print(laptop.total_value()) 
 print(phone.total_value())
